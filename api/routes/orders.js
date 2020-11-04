a0f2ff0775e6074d6db7539cb2233c1dc40d5da0
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-11-04 10:46:05 -0300

    ADD handle error

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,26 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send(200).json({
+  res.status(200).json({
     message: "Orders were fetched",
   });
 });
 
 router.post("/", (req, res, next) => {
-  res.send(201).json({
+  res.status(201).json({
     message: "Orders was created",
   });
 });
 
 router.get("/:orderId", (req, res, next) => {
-  res.send(200).json({
+  res.status(200).json({
     message: "Orders details",
     orderId: req.params.orderId,
   });
 });
 
 router.delete("/:orderId", (req, res, next) => {
-  res.send(200).json({
+  res.status(200).json({
     message: "Orders delete",
     orderId: req.params.orderId,
   });
