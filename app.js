ad0ee231645fe937e97eb9748dd285c9e7f4bb9b
unknown <mateus.bezzan@aluno.unievangelica.edu.br>
2020-11-04 11:41:13 -0300

    ADD bodyParser

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+//Bibliotecas
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
+//express
+const app = express();
+
+// Routes
 const productRouter = require("./api/routes/products");
 const orderRouter = require("./api/routes/orders");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //Routes wich should handle requets
 app.use("/products", productRouter);
